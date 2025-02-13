df6e894eae28f55e56ee10a53a258e7c461f00cf
NateDKing <natedavisking@gmail.com>
2025-02-13 11:05:00 -0500

    Added Example project

--- src/main/java/umlparser/JCUT.java
+++ src/main/java/umlparser/JCUT.java
@@ -5,7 +5,10 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 import org.objectweb.asm.ClassReader;
@@ -19,6 +22,14 @@ public class JCUT {
     public static void main(String[] args) {
         try {
             List<ClassReader> classes = getClassesInPackage();
+            
+            //Empty outputFiles folder
+            Files.walk(Path.of("outputFiles"))
+            .filter(path -> !path.equals(Path.of("outputFiles")))
+            .sorted(Comparator.reverseOrder())
+            .map(Path::toFile)
+            .forEach(File::delete);
+            
             for (ClassReader cr : classes) {
             	ClassNode classNode = new ClassNode(Opcodes.ASM9);
 //            	UMLClassVisitor cv = new UMLClassVisitor(Opcodes.ASM9, classInfo);
@@ -35,7 +46,7 @@ public class JCUT {
                 System.out.println("Writing UML diagram...");
                 System.setProperty("GRAPHVIZ_DOT", "/opt/homebrew/bin/dot");
                 generateSVG(uml, outputPath + ".svg");
-                System.out.println("Wrote UML diagram to diagram.svg");
+                System.out.println("Wrote UML diagram to " + outputPath + ".svg");
             }
             
             
@@ -65,7 +76,7 @@ public class JCUT {
      * Edited from https://stackoverflow.com/questions/28678026/how-can-i-get-all-class-files-in-a-specific-package-in-java
      */
     public static final List<ClassReader> getClassesInPackage() {
-    	String packageName = "build/classes/java/test/umlparser";
+    	String packageName = "bin/test/umlparser";
     	packageName = packageName.replaceAll("/", File.separator);
         
     	File folder = new File(packageName);
